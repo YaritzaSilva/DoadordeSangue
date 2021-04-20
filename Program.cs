8c27a7222aec17f831f20ceeeee3ef60a3594b04
Yaritza <yaritzamicaelleb@gmail.com>
2021-04-20 16:14:35 -0300

    Doador de Sangue

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DoadordeSangue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int idade;
+           string valordigitado;
+
+            Console.WriteLine("--DOADOR DE SANGUE--");
+            Console.WriteLine();
+            Console.Write("Digite sua idade: ");
+            valordigitado= Console.ReadLine();
+            idade= Int32.Parse(valordigitado);
+
+            if(idade>=18 && idade<=67)
+            {
+                Console.WriteLine("Você pode doar sangue, eba!");
+            }
+
+            else
+            {
+                Console.WriteLine("Infelizmente, você não pode doar sangue.");
+            }
+        }
+    }
+}
